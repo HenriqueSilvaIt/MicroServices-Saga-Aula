ca73436a6acb4692eccbe7e1ef1862eb02333ef6
HenriqueSilvaIt <hikysilva2@gmail.com>
2025-08-25 17:23:04 -0300

    Classe de configuração Kafka criada em todos serviços

---
+++ product-validation-service/src/main/java/br/com/microservices/orchestrated/productvalidationservice/config/kafka/KafkaConfig.java
@@ -0,0 +1,147 @@
+package br.com.microservices.orchestrated.productvalidationservice.config.kafka;
+
+import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.core.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+@EnableKafka /*Anotation do Spring Kafka que habilita todos os recursos
+do Kafka na aplicação*/
+@Configuration /* anottation indica ao spring que essa classe vai consumir configurações
+durante a subida do contexto da aplicação
+
+nessa classe de configuração apontamos o
+consumer group-id e auto-offeset-reset que definimos no applicatio.yl do projeto*/
+@RequiredArgsConstructor()
+public class KafkaConfig {
+
+    @Value("${spring.kafka.bootstrap-servers") /*aqui passamos o caminho da propriedade dentro do application.yl
+     se tiver variavel de ambiente ele vai dar prioridade ao valor dela*/
+    private String boostrapServers; /*é o mesmo que passamos no applicatiom.yl*/
+
+    @Value("${spring.kafka.consumer-group-id") /*aqui passamos o caminho da propriedade dentro do application.yl
+    se tiver variavel de ambiente ele vai dar prioridade ao valor dela*/
+    private String grouId;
+
+    @Value("${spring.kafka.consumer-auto-offset-reset") /*aqui passamos o caminho da propriedade dentro do application.yl
+     se tiver variavel de ambiente ele vai dar prioridade ao valor dela*/
+    private String autoOffsetReset;
+
+    @Bean /*Componente do Kafka para o Consumer*/
+    public ConsumerFactory<String, String> consumerFactory() {
+        /* Cosumer Facotory recebe chave valor é uma classe do Spring kafka*/
+
+        return new DefaultKafkaConsumerFactory<>(consumerProps()); /*é um tipo uma implementação do ConsumerFactory
+        vamos ter que definir propridades para ele no método consumerProps abaixo*/
+
+    }
+
+    private Map<String, Object> consumerProps() {
+        /* Map<String, Object> também daria*/
+        var props = new HashMap<String, Object>(); /*variavel recebendo hashMap*/
+
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, boostrapServers); /*chave
+            estamos pegando o nome do servidor da próprio classe ConsumerConfig, e informando o
+            servidor do kafka como valor*/
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, grouId);
+             /*chave
+            estamos pegando o nome do servidor da próprio classe ConsumerConfig, e informando o
+            servidor do kafka como valor*/
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class); /*
+            para enviar ou consumir do Kafka temos que informar o tipo
+             de dados que estamos serializando no caso de em envio(producer) para o kafka
+             ou deserializando no caso de um Consumer, então se estamos consumindo
+             string, tenho que especificar que é string se for outro tipo tem que
+             especificar
+
+             KEY_DESERIALIZER_CLASS_CONFIG aqui é chave que vai ser automtica ou nula
+             n vamos definir
+
+             StringDeserializer.class é uma classe do Kafka*/
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class); /*
+            para enviar ou consumir do Kafka temos que informar o tipo
+             de dados que estamos serializando no caso de em envio(producer) para o kafka
+             ou deserializando no caso de um Consumer, então se estamos consumindo
+             string, tenho que especificar que é string se for outro tipo tem que
+             especificar
+
+             VALUE_DESERIALIZER_CLASS_CONFIG aqui é a chave do valor do payload json
+             que vamos estar consumindo
+
+             StringDeserializer.class é uma classe do Kafka*/
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, autoOffsetReset);/*chave
+            estamos pegando o nome do servidor da próprio classe ConsumerConfig, e informando o
+            servidor do kafka como valor no application.ymlr*/
+
+
+        return props;
+    }
+
+
+    @Bean /*Componente do Kafka para o producer*/
+    public ProducerFactory<String, String> producerFactory() {
+        return new DefaultKafkaProducerFactory<>(producerProps()); /*é um tipo uma implementação do ConsumerFactory
+        vamos ter que definir propridades para ele no método productProps abaixo*/
+    }
+
+    private Map<String, Object> producerProps() {
+        /* Map<String, Object> também daria*/
+        var props = new HashMap<String, Object>(); /*variavel recebendo hashMap*/
+
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, boostrapServers); /*chave
+            estamos pegando o nome do servidor da próprio classe ProducerConfig, e informando o
+            servidor do kafka como valor*/
+             /*chave
+            estamos pegando o nome do servidor da próprio classe ProducerConfig, e informando o
+            servidor do kafka como valor*/
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringDeserializer.class); /*
+            para enviar ou consumir do Kafka temos que informar o tipo
+             de dados que estamos serializando no caso de em envio(producer) para o kafka
+             ou deserializando no caso de um Consumer, então se estamos consumindo
+             string, tenho que especificar que é string se for outro tipo tem que
+             especificar
+
+             KEY_DESERIALIZER_CLASS_CONFIG aqui é chave que vai ser automtica ou nula
+             n vamos definir
+produzindo, é serializar, porque vamos estar serializando a informação
+             StringDeserializer.class é uma classe do Kafka*/
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringDeserializer.class); /*
+            para enviar ou consumir do Kafka temos que informar o tipo
+             de dados que estamos serializando no caso de em envio(producer) para o kafka
+             ou deserializando no caso de um Consumer, então se estamos consumindo
+             string, tenho que especificar que é string se for outro tipo tem que
+             especificar
+
+             VALUE_SERIALIZER_CLASS_CONFIG  aqui é a chave do valor do payload json
+             que vamos estar produzindo, é serializar, porque vamos estar serializando a informação
+
+             StringDeserializer.class é uma classe do Kafka*/
+
+
+        /*auto-offset-reset e group id é configurações só de consumo
+         * quando vamos publicar o tópico n precisamos especificar o grop id
+         * porque publicamos no tópico e o tópico faz a divisão para os grups*/
+
+        return props;
+    }
+
+
+    /*Classe KafkaTemplate que permite instanciar no código e de fato publicar um evento em um tópico*/
+    @Bean
+    public KafkaTemplate<String, String> kafkaTemplate(ProducerFactory<String, String> producerFactory) {
+
+        return new KafkaTemplate<>(producerFactory); /*como criamos o bean producerFactory
+         aqui na classe de configuração, aqui estamos injetando essas configurações no KafkaTemplate
+         que ai quando formos dar um  producerFactory.send("") ele já vai vir com toda essa configuração*/
+    }
+
+}
